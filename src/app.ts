4666e82aa630f1703c3db9e20495869f18694b77
Nemanja Stojanovic <feemail042@gmail.com>
2026-01-13 13:24:15 +0100

    logging & monitoring setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,30 @@
-import express from 'express';
+import express from 'express'
+import helmet from 'helmet'
+import logger from '#config/logger.ts'
+import morgan from 'morgan'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+const app = express()
 
-const app = express();
+app.use(helmet())
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+type Message = string
+
+app.use(
+  morgan('combined', {
+    stream: {
+      write: (message: Message) => logger.info(message.trim()),
+    },
+  })
+)
 
 app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Hello from the application',
-  });
-});
+  logger.info('Hello from the application')
+  res.send('Hello from the application')
+})
 
-export default app;
+export default app
